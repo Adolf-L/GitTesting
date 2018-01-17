24c5300e28a35a370c4ff1eadcbfb7f20ffcf102
Adolf <rnthking@icloud.com>
2018-01-17 19:59:57 +0800

    Initial Commit

---
+++ GitTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTesting
+//
+//  Created by Rnthking on 2018/1/17.
+//  Copyright © 2018年 Adolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTesting
+//
+//  Created by Rnthking on 2018/1/17.
+//  Copyright © 2018年 Adolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
